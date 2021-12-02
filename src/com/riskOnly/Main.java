16d6d31f66d1544dfc4b147b6d530824a40cd8ec
LuVogel <lukas.vogel@stud.unibas.ch>
2021-12-02 16:02:20 +0100

    add runtime test

--- src/com/riskOnly/Main.java
+++ src/com/riskOnly/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        matrixGenerator = new MatrixGenerator(7, 3);
+        matrixGenerator = new MatrixGenerator(35, 35);
         int attacker = matrixGenerator.getAttacker();
         int defender = matrixGenerator.getDefender();
 
